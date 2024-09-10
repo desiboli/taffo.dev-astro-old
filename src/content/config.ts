a8f7801f130b5c847e60ba37047f620cb95f91cf
desiboli <adam.benals@gmail.com>
2024-09-10 23:09:12 +0200

    projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from "astro:content"
 
 const blog = defineCollection({
   type: "content",
@@ -11,6 +11,21 @@ const blog = defineCollection({
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
-});
+})
 
-export const collections = { blog };
+const project = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    logo: z.string().optional(),
+    color: z.string().optional(),
+  }),
+})
+
+export const collections = { blog, project }
